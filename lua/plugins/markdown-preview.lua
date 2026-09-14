f73f83c350295eab2741bb8f11b645ca4efbb9aa
Mulberry <lisiwodei@gmail.com>
2026-09-14 18:17:46 +0800

    fix(markdown-preview): fixed the issue of markdown-preview update failures
    - Replace `--prefix` with `cd /d` to fix npm install issues on Windows
    - Add local git exclude rule to prevent Lazy.nvim sync dirty yarn.lock

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -4,14 +4,28 @@ return {
 	"iamcco/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 	ft = { "markdown" },
+
 	build = function()
-		if vim.fn.executable("npm") == 0 then
-			vim.notify("[markdown-preview] npm not found, skipping build", vim.log.levels.WARN)
+		local npm = vim.fn.executable("npm.cmd") == 1 and "npm.cmd" or vim.fn.executable("npm") == 1 and "npm" or nil
+		if not npm then
+			vim.schedule(function()
+				vim.notify("[markdown-preview] npm not found, skipping build", vim.log.levels.ERROR)
+			end)
 			return
 		end
+
 		local plugin_dir = utils.get_plugin_dir("markdown-preview.nvim")
-		vim.fn.system({ "npm", "install", "--prefix", plugin_dir })
+		local exclude_file = vim.fs.joinpath(plugin_dir, ".git", "info", "exclude")
+		local f = io.open(exclude_file, "a")
+		if f then
+			f:write("\nyarn.lock\n")
+			f:close()
+		end
+
+		local cmd = string.format('cd /d "%s" && %s install', plugin_dir, npm)
+		vim.fn.system(cmd)
 	end,
+
 	config = function()
 		vim.g.mkdp_highlight_current_line = 1
 	end,
