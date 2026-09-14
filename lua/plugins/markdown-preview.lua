565ed4057ef5db6536554abda98f4f84e05453e7
Mulberry <lisiwodei@gmail.com>
2026-09-15 01:29:00 +0800

    refactor(markdown-preview): use yarn to build markdown-preview
    - change package install cmd to fit pwsh
    - use yarn install to avoid dirty yarn.lock
    - use plugin.dir in build function to replace utils.get_plugin_dir

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,29 +1,24 @@
-local utils = require("custom/utils")
-
 return {
 	"iamcco/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 	ft = { "markdown" },
 
-	build = function()
-		local npm = vim.fn.executable("npm.cmd") == 1 and "npm.cmd" or vim.fn.executable("npm") == 1 and "npm" or nil
-		if not npm then
+	build = function(plugin)
+		local yarn_path = vim.fn.exepath("yarn.cmd")
+		if yarn_path == "" then
 			vim.schedule(function()
-				vim.notify("[markdown-preview] npm not found, skipping build", vim.log.levels.ERROR)
+				vim.notify("[markdown-preview] yarn is not installed", vim.log.levels.ERROR)
 			end)
 			return
 		end
 
-		local plugin_dir = utils.get_plugin_dir("markdown-preview.nvim")
-		local exclude_file = vim.fs.joinpath(plugin_dir, ".git", "info", "exclude")
-		local f = io.open(exclude_file, "a")
-		if f then
-			f:write("\nyarn.lock\n")
-			f:close()
+		local cmd = string.format('cd "%s"; & "%s" install', plugin.dir, yarn_path)
+		local output = vim.fn.system(cmd)
+		if vim.v.shell_error ~= 0 then
+			vim.schedule(function()
+				vim.notify("[markdown-preview] yarn install failed: " .. output, vim.log.levels.ERROR)
+			end)
 		end
-
-		local cmd = string.format('cd /d "%s" && %s install', plugin_dir, npm)
-		vim.fn.system(cmd)
 	end,
 
 	config = function()
