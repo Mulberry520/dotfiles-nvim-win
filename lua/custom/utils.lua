33196a8fec634ff01f83018f1c189f8c30cd75fa
Mulberry <lisiwodei@gmail.com>
2026-09-15 01:44:04 +0800

    refactor(custom.utils): use vim api to get plugin dir

--- lua/custom/utils.lua
+++ lua/custom/utils.lua
@@ -1,18 +1,8 @@
 local M = {}
 
 function M.get_plugin_dir(plugin_name)
-	local ok, lazy = pcall(require, "lazy.core.config")
-	if not ok or type(lazy.plugins) ~= "table" then
-		return vim.fs.joinpath(vim.fn.stdpath("data"), "lazy", plugin_name)
-	end
-
-	for _, spec in ipairs(lazy.plugins) do
-		if spec.name == plugin_name then
-			return spec.dir
-		end
-	end
-
-	return vim.fs.joinpath(vim.fn.stdpath("data"), "lazy", plugin_name)
+	local lazy_config = require("lazy.core.config")
+	return lazy_config.plugins[plugin_name] and lazy_config.plugins[plugin_name].dir or nil
 end
 
 return M
